8efa7f42aa31bde4026b9d239e64c3a31a66244f
joaoperfig <joaoperfig@gmail.com>
2021-12-16 04:34:27 +0000

    update release link

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ setup(
   packages = ['widelinears'],
   version = '0.1',
   license='MIT',
-  description = 'Parallel pytorch Neural Networks',   # Give a short description about your library
-  author = 'Joao Figueira',                   # Type in your name
-  author_email = 'joaoperfig@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/joaoperfig/WideLinears',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/joaoperfig/WideLinears/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['pytorch', 'parallel', 'linear'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  description = 'Parallel pytorch Neural Networks',   
+  author = 'Joao Figueira',                  
+  author_email = 'joaoperfig@gmail.com',   
+  url = 'https://github.com/joaoperfig/WideLinears', 
+  download_url = 'https://github.com/joaoperfig/WideLinears/archive/refs/tags/v_0.1.tar.gz',  
+  keywords = ['pytorch', 'parallel', 'linear'], 
+  install_requires=[          
           'torch',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',   
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',    
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
